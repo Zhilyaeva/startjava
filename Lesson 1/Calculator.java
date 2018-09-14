99cc350b020e69c75fc9c201312719b1c8dfba35
Ира <zhilyaeva@MacBook-Pro.local>
2018-09-14 20:14:13 +0300

    Исправила в соответствии с комментариями преподавателя

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -1,31 +1,27 @@
 public class Calculator {
-public static void main(String[] args) {
-int x = 15;
-int y = 3;
-char mathem = '^';
+	public static void main(String[] args) {
+	int x = 15;
+	int y = 3;
+	char mathFunction = '^';
 
-if (mathem == '+'){
-	System.out.println(x + y);
-}
-else if (mathem == '-'){
-	System.out.println(x - y);
-}
-else if (mathem == '*'){
-	System.out.println(x * y);
-}
-else if (mathem == '/'){
-	System.out.println(x / y);
-}
-else if (mathem == '^'){
-	int result = 1;
-	for (int i = 1; i <= y; i++){
-		result = result*x;
+		if (mathFunction == '+') {
+			System.out.println(x + y);
+		} else if (mathFunction == '-') {
+			System.out.println(x - y);
+		} else if (mathFunction == '*') {
+			System.out.println(x * y);
+		} else if (mathFunction == '/') {
+			System.out.println(x / y);
+		} else if (mathFunction == '^') {
+ 		
+ 		int result = 1;
+		for (int i = 1; i <= y; i++) {
+		result *= x;
+		}
+			System.out.println(result);
+		} else if (mathFunction == '%') {
+			System.out.println(x % y);
+		}
 	}
-	System.out.println(result);
-}
-else if (mathem == '%'){
-	System.out.println(x % y);
-}
-}
 	
 }
--- Lesson 1/MyFirstGame.java
+++ Lesson 1/MyFirstGame.java
@@ -1,21 +1,20 @@
 import java.util.Scanner; // Было в задачах в материалах про ветвление в Java
+
 public class MyFirstGame {
 	public static void main(String[] args) {
-		int magic = 99;
-		System.out.print ("Введи число от 1 до 100: "); 
-		Scanner  num = new Scanner (System.in); 
-		int i = num.nextInt (); 
-		if (i == magic) {
-			System.out.println ("Ты угадал. Ты сильное звено!");
-		}
-		else if ( i < magic && i >= 1 ) {
-			System.out.println ("Мое число больше");
-		}
-		else if ( i > magic && i <= 100 ) {
-			System.out.println ("Мое число меньше");
-		}
-		else {
-			System.out.println ("Засранец! Играй по правилам!");
+		int secretNumber = 99;
+		System.out.print("Введи число от 0 до 100: "); 
+		Scanner answer = new Scanner(System.in); 
+		int answerNumber = answer.nextInt(); 
+
+		if (answerNumber == secretNumber) {
+			System.out.println("Ты угадал. Ты сильное звено!");
+		} else if ( answerNumber < secretNumber && answerNumber >= 0 ) {
+			System.out.println("Мое число больше");
+		} else if ( answerNumber > secretNumber && answerNumber <= 100 ) {
+			System.out.println("Мое число меньше");
+		} else {
+			System.out.println("Засранец! Играй по правилам!");
 		}
 	}
 }
