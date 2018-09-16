394ded5cd73a74660edf1f3517130c1b325d31ff
Ира <zhilyaeva@MacBook-Pro.local>
2018-09-16 15:51:18 +0300

    Отформатированный код.

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -1,23 +1,22 @@
 public class Calculator {
 	public static void main(String[] args) {
-	int x = 15;
-	int y = 3;
-	char mathOperation = '^';
+		int x = 15;
+		int y = 3;
+		char mathOperation = '^';
 
-	if (mathOperation == '+') {
-		System.out.println(x + y);
-	} else if (mathOperation == '-') {
-		System.out.println(x - y);
-	} else if (mathOperation == '*') {
-		System.out.println(x * y);
-	} else if (mathOperation == '/') {
-		System.out.println(x / y);
-	} else if (mathOperation == '^') {
- 		
- 		int result = 1;
-		for (int i = 1; i <= y; i++) {
-			result *= x;
-		}
+		if (mathOperation == '+') {
+			System.out.println(x + y);
+		} else if (mathOperation == '-') {
+			System.out.println(x - y);
+		} else if (mathOperation == '*') {
+			System.out.println(x * y);
+		} else if (mathOperation == '/') {
+			System.out.println(x / y);
+		} else if (mathOperation == '^') {
+ 			int result = 1;
+			for (int i = 1; i <= y; i++) {
+				result *= x;
+			}
 			System.out.println(result);
 		} else if (mathOperation == '%') {
 			System.out.println(x % y);
