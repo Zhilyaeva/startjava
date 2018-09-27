797266781c9695b1c0a3379c9dcdf9544e494d4e
Ира <zhilyaeva@MacBook-Pro.local>
2018-09-27 21:55:53 +0300

    Переделанный калькулятор

--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -2,27 +2,31 @@ import java.util.Scanner;
 
 public class CalculatorTest {
 	public static void main(String[] args) {
-		Calculator calculator = new Calculator(); 
-		String response;  
+		Calculator calculator = new Calculator();
 		Scanner scan = new Scanner(System.in); 
+		Scanner scanLine = new Scanner(System.in);
+		String response;
 
 		System.out.println ("Программа запущена.");
-		
+
 		do {
 			System.out.print("Введите первое число: ");
 			calculator.setFirstNumber(scan.nextFloat());
 
 			System.out.print("Введите знак математической операции: ");
-			calculator.setMathOperation(scan.next());
+			calculator.setMathOperation(scanLine.nextLine());
 
 			System.out.print("Введите второе число: ");
 			calculator.setSecondNumber(scan.nextFloat());
 		
+			System.out.print("Ответ: ");
 			calculator.solution();
 
+			do {
 			System.out.print("Вы хотите продолжить?: ");
-			response = scan.next(); 
+			response = scanLine.nextLine();
+			} while (!response.equals("да") && !response.equals("нет"));
 
-		} while (response.equals("да"));
+		} while (!response.equals("нет"));
 	}
 }
