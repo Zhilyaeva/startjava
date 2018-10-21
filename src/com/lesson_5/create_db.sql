818855980d99938d57664b00426f5122add4e2a0
zhilyaeva <irarunina@gmail.com>
2018-10-21 21:41:32 +0300

    5 Урок

---
+++ src/com/lesson_5/create_db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Jaegers (
+id SERIAL PRIMARY KEY,
+modelName TEXT,
+mark TEXT,
+height REAL,
+weight REAL,
+status TEXT,
+origin TEXT,
+launch DATE,
+kaijuKill INTEGER
+);
---
+++ src/com/lesson_5/querise.sql
@@ -0,0 +1,18 @@
+SELECT * FROM Jaegers;
+
+SELECT * FROM Jaegers WHERE status = 'Not destroed';
+
+SELECT * FROM Jaegers WHERE mark = 'Mark-1' OR mark = 'Mark-6';
+
+SELECT * FROM Jaegers ORDER BY mark DESC;
+
+SELECT * FROM Jaegers WHERE launch = (SELECT MIN(launch) FROM Jaegers);
+
+SELECT * FROM Jaegers WHERE kaijuKill = (SELECT MAX(kaijuKill) FROM Jaegers);
+
+SELECT AVG(weight) FROM Jaegers;
+
+UPDATE Jaegers SET kaijuKill = kaijuKill + 1 WHERE status = 'Not destroed';
+
+DELETE FROM Jaegers WHERE status = 'Destroed';
+
